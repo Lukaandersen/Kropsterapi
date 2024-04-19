654bc18efb1756692058956503e288b4bf62b618
magnuslykke <magn965m@stud.kea.dk>
2024-04-19 10:17:15 +0200

    knapt

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,99 +1,91 @@
-
-'use client';
-import { builder, Builder } from '@builder.io/react';
-import Counter from './components/blocks/Counter/Counter';
-import Footer from './components/blocks/Footer/Footer';
-import Testamonial from './components/blocks/Testamonials/Testamonial';
-import Navigation from './components/blocks/Navigation/Navigation'
+"use client";
+import { builder, Builder } from "@builder.io/react";
+import Counter from "./components/blocks/Counter/Counter";
+import Footer from "./components/blocks/Footer/Footer";
+import Testamonial from "./components/blocks/Testamonials/Testamonial";
+import Navigation from "./components/blocks/Navigation/Navigation";
+import BookingButton from "./components/buttons/BookingButton/BookingButton";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 Builder.registerComponent(Counter, {
-    name: 'Counter',
-    inputs: [
+  name: "Counter",
+  inputs: [
+    {
+      name: "initialCount",
+      type: "number",
+    },
+    {
+      name: "reviews",
+      type: "list",
+      defaultValue: [{ reviewText: "hello" }],
+      showIf: (options) => {
+        return options.get("initialCount") > 50;
+      },
+      subFields: [
         {
-            name: 'initialCount',
-            type: 'number',
+          name: "reviewText",
+          type: "string",
+          defaultValue: '"You are the best"',
         },
         {
-            name: 'reviews',
-            type: 'list',
-            defaultValue: [{ reviewText: 'hello' }],
-            showIf: (options) => {
-                return options.get('initialCount') > 50;
-            },
-            subFields: [
-                {
-                    name: 'reviewText',
-                    type: 'string',
-                    defaultValue: '"You are the best"',
-                },
-                {
-                    name: 'reviewAuthor',
-                    type: 'string',
-                    defaultValue: 'Jane Smith',
-                },
-                {
-                    name: 'image',
-                    type: 'file',
-                    allowedFileTypes: ['jpeg', 'jpg', 'png', 'svg'],
-                    required: true,
-                   
-                },
-            ],
+          name: "reviewAuthor",
+          type: "string",
+          defaultValue: "Jane Smith",
         },
-    ],
+        {
+          name: "image",
+          type: "file",
+          allowedFileTypes: ["jpeg", "jpg", "png", "svg"],
+          required: true,
+        },
+      ],
+    },
+  ],
 });
 
 Builder.registerComponent(Footer, {
-    name: 'Footer',
-  
-          
-    
+  name: "Footer",
 });
 Builder.registerComponent(Testamonial, {
-    name: 'Testamonial',
-    inputs: [
-        {
-            name:'name',
-            type:'string',
-
-        },
-        {
-            name:'title',
-            type:'string',
-        },
-        {
-            name:'testamonialText',
-            type:'string'
-        }
-    ]
-          
-    
+  name: "Testamonial",
+  inputs: [
+    {
+      name: "name",
+      type: "string",
+    },
+    {
+      name: "title",
+      type: "string",
+    },
+    {
+      name: "testamonialText",
+      type: "string",
+    },
+  ],
 });
 
 Builder.registerComponent(Navigation, {
-    name: 'Navigation',
-    inputs: [
-        {
-            name:'menupunkt1',
-            type:'string',
-
-        },
-        {
-            name:'menupunkt2',
-            type:'string',
-        },
-        {
-            name:'menupunkt3',
-            type:'string'
-        },
-        {
-            name:'logo',
-            type:'image'
-        }
-    ]
-          
-    
+  name: "Navigation",
+  inputs: [
+    {
+      name: "menupunkt1",
+      type: "string",
+    },
+    {
+      name: "menupunkt2",
+      type: "string",
+    },
+    {
+      name: "menupunkt3",
+      type: "string",
+    },
+    {
+      name: "logo",
+      type: "image",
+    },
+  ],
 });
-
+Builder.registerComponent(BookingButton, {
+  name: "BookingButton",
+});
---
+++ components/buttons/BookingButton/BookingButton.tsx
@@ -0,0 +1,3 @@
+export default function BookinButton() {
+  return <h1 className="text-primaryPurple font-josefin font-bold text-3xl">Jeg er en knapt</h1>;
+}
