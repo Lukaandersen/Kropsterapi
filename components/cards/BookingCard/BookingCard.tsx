8f33d2a03cc9fd415e0e92ed9b958f0cf7cfc81f
magnuslykke <magn965m@stud.kea.dk>
2024-04-22 15:40:20 +0200

    large lort

--- components/cards/BookingCard/BookingCard.tsx
+++ components/cards/BookingCard/BookingCard.tsx
@@ -6,8 +6,8 @@ export default function BookingCard(props) {
     <div className="bg-darkBeige mx-6 my-2 flex flex-col items-center justify-center">
       <p className="m-5 text-primaryLight">{props.titel}</p>
       <img src="3_sessioner.png" alt="Billede af noget spirituelt" className="w-64" />
-      <p className="text-center text-5xl m-5 text-primaryLight">{props.text}</p>
-      <p className="text-3xl font-bold  !important">{props.price}</p>
+      <p className="text-center text-lg m-5 text-primaryLight">{props.text}</p>
+      <p className="text-5xl font-bold  !important">{props.price}</p>
       <BrownButton></BrownButton>
     </div>
   );
