39928830b11d5db968ecd44ad837dc814fe75c07
magnuslykke <magn965m@stud.kea.dk>
2024-04-25 12:19:49 +0200

    Booking og CTA

--- components/blocks/CTA-Banner/CTABanner.tsx
+++ components/blocks/CTA-Banner/CTABanner.tsx
@@ -1,7 +1,7 @@
 export default function CTABanner(props) {
   return (
-    <div className="bg-mediumBeige p-10 flex flex-col items-center justify-center">
-      <p className="text-3xl">{props.titel}</p>
+    <div className="bg-mediumBeige p-10 flex  text-primaryPurple flex-col items-center justify-center">
+      <p className="text-[32px]">{props.titel}</p>
     </div>
   );
 }
