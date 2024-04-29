77cf43443677ab2bd6d9ac35799db61ac7976b9d
Luka Andersen <luka_andersen@hotmail.com>
2024-04-29 13:14:17 +0200

    dark/lightmode

--- app/[[...page]]/page.tsx
+++ app/[[...page]]/page.tsx
@@ -1,13 +1,40 @@
 "use client"
+import React, { useState, useEffect } from 'react';
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from '@/components/builder';
+import Navigation from '@/components/blocks/Navigation/Navigation';
+import Footer from '@/components/blocks/Footer/Footer';
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-import PageContent from "@/components/PageContent";
-import Footer from "@/components/blocks/Footer/Footer";
-import Navigation from "@/components/blocks/Navigation/Navigation";
 export default function Page() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [content, setContent] = useState(null);
+  const [darkModeButtonImage, setDarkModeButtonImage] = useState('DarkModeButton.png');
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    setDarkModeButtonImage(darkMode ? 'DarkModeButton.png' : 'LightModeButton.png');
+  };
+
+  useEffect(() => {
+    async function fetchContent() {
+      const pageContent = await builder.get("page").toPromise();
+      setContent(pageContent);
+    }
+    fetchContent();
+  }, []);
+
+  if (!content) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="bg-lightBeige">
+    <div className={`bg-${darkMode ? 'darkBeige' : 'lightBeige'}`}style={{ position: 'relative'}}>
       <Navigation />
-      <PageContent />
+      <button onClick={toggleDarkMode} className='absolute z-50'>
+      <img src={darkModeButtonImage} alt="" width={45}/>
+      </button>
+      <RenderBuilderContent content={content}/>
       <Footer />
     </div>
   );
