a6e3d87480ec15ba28ec82cfc7f16bc4725ff6dd
magnuslykke <magn965m@stud.kea.dk>
2024-04-29 11:08:11 +0200

    timeline

--- components/blocks/Timeline/Timeline.tsx
+++ components/blocks/Timeline/Timeline.tsx
@@ -1,21 +1,23 @@
+// export default function Timeline(props) {
+//   return (
+//     <div className="bg-primaryLight w-auto min-h-14">
+//       <div>
+//         <p className="text-xl text-darkBeige">Tidslinje</p>
+//       </div>
+//       <img className="md:hidden position:absolut top--100px" src="lightmode_mobile_timeline.png" alt="Tidslinje mobile" />
+//       <img className="hidden sm:block " src="lightmode_desk_timeline.png" alt="Tidslinje desktop" />
+//     </div>
+//   );
+// }
+
 export default function Timeline(props) {
   return (
-    <div className="bg-darkBeige w-auto min-h-14">
+    <div className="bg-primaryLight w-auto min-h-14 relative">
       <div>
-        <p className="text-xl text-primaryLight">Uddannet Reiki 1</p>
+        <p className="text-xl text-darkBeige">Tidslinje</p>
       </div>
-      <div>
-        <p>Uddannet ved Totum</p>
-      </div>
-      <div>
-        <p>Uddannet i anatomi og sygdomlære</p>
-      </div>
-      <div>
-        <p>Egen klinik</p>
-      </div>
-      <div>
-        <p>Uddannet Reiki 2</p>
-      </div>
+      <img className="md:hidden ml-3" src="lightmode_mobile_timeline.png" alt="Tidslinje mobile" />
+      <img className="hidden sm:block mt-[-250px]" src="lightmode_desk_timeline.png" alt="Tidslinje desktop" />
     </div>
   );
 }
