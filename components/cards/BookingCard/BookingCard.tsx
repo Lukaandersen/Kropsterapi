4fbbb9de4b3140db24c7ed45087fea8cebf33b45
magnuslykke <magn965m@stud.kea.dk>
2024-05-01 13:47:40 +0200

    farveændirng

--- components/cards/BookingCard/BookingCard.tsx
+++ components/cards/BookingCard/BookingCard.tsx
@@ -28,7 +28,7 @@ function BookingCard(props) {
         <p className={`m-0 text-[26px] ${darkMode ? " text-primaryPurple" : "text-primaryPurple md:text-primaryLight"} `}>{props.titel}</p>
         <div className="relative mx-auto text-center w-64">
           <img src="3_sessioner.png" alt="Billede af noget spirituelt" className="w-full h-auto opacity-35 md:mx-auto md:w-[200px] " />
-          <p className={`absolute top-0 left-0 right-0 bottom-0 md:relative flex items-center justify-center text-center text-md md:m-5 md:mt-0 ${darkMode ? "text-primaryPurple" : "text-primaryLight"}`}>{props.text}</p>
+          <p className={`absolute top-0 left-0 right-0 bottom-0 md:relative flex items-center justify-center text-center text-md md:m-5 md:mt-0 ${darkMode ? "md:text-primaryPurple text-primaryLight" : "text-primaryLight"}`}>{props.text}</p>
         </div>
         <p className="text-[32px] font-bold">{props.price}</p>
         <BrownButton></BrownButton>
