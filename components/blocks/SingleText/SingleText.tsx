8261198ef9e8dac92394a41e5f550e14b4d7d19d
Luka Andersen <luka_andersen@hotmail.com>
2024-05-02 12:06:50 +0200

    feat: Gallery function

--- components/blocks/SingleText/SingleText.tsx
+++ components/blocks/SingleText/SingleText.tsx
@@ -2,9 +2,9 @@ import LightBeigeButton from "@/components/buttons/LightBeigeButton";
 
 export default function SingleText (props) {
     return (
-        <div className="grid py-5 md:px-28 place-items-center text-primaryPurple">
+        <div className="grid py-5 md:px-28 place-items-center px-5 text-primaryPurple">
             <h2 className="text-3xl font-playfair mb-5">{props.title}</h2>
-                <p className="font-josefin">Ankomst : Jeg gør alt jeg kan for at være klar til dig, være 100% nærværende og tilstede, 1 time derikeret til dig.  Du skal ankomme ca 5 min før behandlingstid så en eventuel tidligere behandling ikke forstyres men så du kan nå at være klar til din tid. </p> 
+                <p className="font-josefin">Ankomst : Jeg gør alt jeg kan for at være klar til dig, være 100% nærværende og tilstede, 1 time dedikeret til dig.  Du skal ankomme ca 5 min før behandlingstid så en eventuel tidligere behandling ikke forstyres men så du kan nå at være klar til din tid. </p> 
                 <br />
                 <p>Medbring: Du skal medbringe et lagen, gerne stræklagen da de ligger bedst på briksen og et tæppe af ikke alt for tyk kvalitet som skal bruges til at dække de dele af kroppen som ikke behandles. </p> 
                 <br />
