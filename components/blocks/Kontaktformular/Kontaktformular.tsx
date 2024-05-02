d420bbb7f47bdf806cdc2a165bb6ccde1b83dcd0
magnuslykke <magn965m@stud.kea.dk>
2024-05-02 10:54:07 +0200

    kontakt farve tekst

--- components/blocks/Kontaktformular/Kontaktformular.tsx
+++ components/blocks/Kontaktformular/Kontaktformular.tsx
@@ -3,31 +3,31 @@ import LightBeigeButton from "@/components/buttons/LightBeigeButton";
 export default function Kontaktformular() {
   return (
     <div className="m-9">
-      <h2 className="text-center md:text-left text-[40px] pb-6 md:pl-2 font-playfair">Kontakt</h2>
-      <p className="pb-6 md:pl-2">Fortæl lidt om dig selv og din situation - så tager jeg kontakt til dig</p>
+      <h2 className="text-center md:text-left text-[40px] pb-6 md:pl-2 font-playfair text-primaryPurple">Kontakt</h2>
+      <p className="pb-6 md:pl-2 text-primaryPurple">Fortæl lidt om dig selv og din situation - så tager jeg kontakt til dig</p>
       <form className="flex flex-col gap-4 bg-primaryLight rounded-lg p-5">
         <div className="flex flex-col md:px-[70px]">
-          <label htmlFor="name" className="">
+          <label htmlFor="name" className="text-primaryPurple">
             Navn
           </label>
           <input type="text" id="name" className=" p-2 bg-gray-300 " placeholder="Indtast dit navn" />
         </div>
         <div className=" md:flex md:justify-between md:px-[70px]">
           <div className="flex flex-col ">
-            <label htmlFor="email" className="">
+            <label htmlFor="email" className="text-primaryPurple">
               E-mail
             </label>
             <input type="email" id="email" className="p-2 bg-gray-300 md:w-[250px]" placeholder="Indtast din e-mailadresse" />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="phone" className="pt-4 md:pt-0">
+            <label htmlFor="phone" className="pt-4 md:pt-0 text-primaryPurple">
               Tlf nr
             </label>
             <input type="tel" id="phone" className=" p-2 bg-gray-300 md:w-[250px]" placeholder="Indtast dit telefonnummer" />
           </div>
         </div>
         <div className="flex flex-col md:px-[70px]">
-          <label htmlFor="message" className="">
+          <label htmlFor="message" className="text-primaryPurple">
             Besked
           </label>
           <textarea id="message" className=" p-2 bg-gray-300" placeholder="Indtast din besked"></textarea>
