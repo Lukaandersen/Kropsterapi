3faed0736e58a25b55d7b879357a512429e41760
magnuslykke <magn965m@stud.kea.dk>
2024-05-06 10:23:44 +0200

    csss

--- components/blocks/MinFormular/MinFormular.tsx
+++ components/blocks/MinFormular/MinFormular.tsx
@@ -20,21 +20,23 @@ export default function MinFormular() {
   };
 
   return (
-    <div className="container mx-auto mt-[300px] px-4">
-      <h1 className="text-2xl font-semibold mb-4">Bookingformular</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <label className="mb-2">
-          Dato:
-          <input type="date" value={selectedDate} onChange={handleDateChange} className="border border-gray-300 rounded-md p-2 ml-2" />
-        </label>
-        <label className="mb-2">
-          Tidspunkt:
-          <input type="time" value={selectedTime} onChange={handleTimeChange} className="border border-gray-300 rounded-md p-2 ml-2" />
-        </label>
-        <button type="submit" className="bg-darkBeige text-white font-semibold py-2 px-4 rounded-md mt-4 hover:bg-blue-600 transition duration-300">
-          Indsend
-        </button>
-      </form>
+    <div>
+      <div className="container mx-auto mt-[300px] px-4 bg-lightBeige">
+        <h1 className="text-2xl font-semibold mb-4">Bookingformular</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+          <label className="mb-2">
+            Dato:
+            <input type="date" value={selectedDate} onChange={handleDateChange} className="border border-gray-300 rounded-md p-2 ml-2" />
+          </label>
+          <label className="mb-2">
+            Tidspunkt:
+            <input type="time" value={selectedTime} onChange={handleTimeChange} className="border border-gray-300 rounded-md p-2 ml-2" />
+          </label>
+          <button type="submit" className="bg-darkBeige text-white font-semibold py-2 px-4 rounded-md mt-4 hover:bg-primaryPurple transition duration-300">
+            Indsend
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
