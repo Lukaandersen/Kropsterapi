15068d32d9aa3bb31304710f94c91c86c93defb4
magnuslykke <magn965m@stud.kea.dk>
2024-05-06 12:40:31 +0200

    stylingsdut

--- components/blocks/MinFormular/MinFormular.tsx
+++ components/blocks/MinFormular/MinFormular.tsx
@@ -1,36 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
 export default function MinFormular() {
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedTime, setSelectedTime] = useState("");
 
-  const handleDateChange = (event) => {
+  const handleDateChange = (event: any) => {
     setSelectedDate(event.target.value);
   };
 
-  const handleTimeChange = (event) => {
+  const handleTimeChange = (event: any) => {
     setSelectedTime(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = (event: any) => {
     event.preventDefault();
     // Her kan du håndtere den indsendte formulardata, f.eks. sende det til en API eller en anden funktion.
-    console.log("Dato:", selectedDate);
+    console.log("Dato:+hej", selectedDate);
     console.log("Tidspunkt:", selectedTime);
   };
 
   return (
     <div>
-      <div className="container mx-auto mt-[300px] px-4 bg-lightBeige">
-        <h1 className="text-2xl font-semibold mb-4">Bookingformular</h1>
+      <div className="container mx-auto mt-[300px] px-4 bg-lightBeige py-[100px]">
+        <h1 className="text-2xl text-center font-semibold mb-4 text-primaryPurple">Bookingformular</h1>
         <form onSubmit={handleSubmit} className="flex flex-col items-center">
           <label className="mb-2">
             Dato:
-            <input type="date" value={selectedDate} onChange={handleDateChange} className="border border-gray-300 rounded-md p-2 ml-2" />
+            <input name="date" type="date" value={selectedDate} onChange={handleDateChange} className="border border-gray-300 rounded-md p-2 ml-2 w-[200px]" />
           </label>
           <label className="mb-2">
             Tidspunkt:
-            <input type="time" value={selectedTime} onChange={handleTimeChange} className="border border-gray-300 rounded-md p-2 ml-2" />
+            <input name="time" type="time" value={selectedTime} onChange={handleTimeChange} className="border border-gray-300 rounded-md p-2 ml-2 w-[200px]" />
           </label>
           <button type="submit" className="bg-darkBeige text-white font-semibold py-2 px-4 rounded-md mt-4 hover:bg-primaryPurple transition duration-300">
             Indsend
