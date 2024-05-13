320be0048d9d59bffae83dd42b67bc99bc2ac6e2
magnuslykke <magn965m@stud.kea.dk>
2024-05-13 12:30:51 +0200

    timeslot

---
+++ components/blocks/Cart/Timeslot.tsx
@@ -0,0 +1,12 @@
+export default function Timeslot() {
+  return (
+    <div>
+      <h1
+        className="bg-blue-300 h-[350px]
+        "
+      >
+        Dette er en timeslot din Slut
+      </h1>
+    </div>
+  );
+}
