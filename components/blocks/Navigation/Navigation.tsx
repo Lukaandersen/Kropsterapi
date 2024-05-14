c5b4a52a588eb27797ef17b8ec701f72d3df2f00
magnuslykke <magn965m@stud.kea.dk>
2024-05-14 14:59:19 +0200

    navigation

--- components/blocks/Navigation/Navigation.tsx
+++ components/blocks/Navigation/Navigation.tsx
@@ -1,42 +1,48 @@
-import { useState } from 'react';
+import { useState } from "react";
 import styles from "./styles.module.css";
-import BurgerMenu from './BurgerMenu';
+import BurgerMenu from "./BurgerMenu";
 
 export default function Navigation() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-        console.log("åben")
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log("åben");
+  };
 
-    return (
-        <div className='bg-lightBeige/50 flex place-items-center justify-between z-40 sticky top-0'>
-            <div>
-                <a href="/forside">
-                    <img src="Logo.png" alt="" />
-                </a>
-            </div>
-            <div className='flex gap-7'>
-            <div className="md:hidden justify-center  " onClick={toggleMenu}>
-                <div className={`${styles.burgermenu} ${isOpen ? styles.open : ''}`}>
-                    <div className={`${styles.line} ${styles.line1}`}></div>
-                    <div className={`${styles.line} ${styles.line2}`}></div>
-                    <div className={`${styles.line} ${styles.line3}`}></div>
-                </div>
-                <BurgerMenu isOpen={isOpen} />
-            </div>
-            <ul className={`md:flex gap-6 text-primaryPurple mr-4 ${isOpen ? 'hidden ' : 'hidden'}`}>
-                <li className='hover:text-darkBlue font-semibold'><a href="/booking">Booking</a></li>
-                <li className='hover:text-darkBlue font-semibold'><a href="/about-me">Om mig</a></li>
-                <li className='hover:text-darkBlue font-semibold'><a href="/hvad-er-kropsterapi">Hvad er Kropsterapi?</a></li>
-            </ul>
-            <div>
-                <a href="/cart">
-                    <img src="Cart.png" alt="" />
-                </a>
-            </div>
-            </div>
+  return (
+    <div className="bg-lightBeige/50 flex place-items-center justify-between z-40 sticky top-0">
+      <div>
+        <a href="/forside">
+          <img src="Logo.png" alt="" />
+        </a>
+      </div>
+      <div className="flex gap-7 mr-5 md:mr-[50px]">
+        <ul className={`md:flex gap-6 text-primaryPurple mr-4 ${isOpen ? "hidden " : "hidden"}`}>
+          <li className="hover:text-darkBlue font-semibold">
+            <a href="/booking">Booking</a>
+          </li>
+          <li className="hover:text-darkBlue font-semibold">
+            <a href="/about-me">Om mig</a>
+          </li>
+          <li className="hover:text-darkBlue font-semibold">
+            <a href="/hvad-er-kropsterapi">Hvad er Kropsterapi?</a>
+          </li>
+        </ul>
+        <div>
+          <a href="/cart">
+            <img src="Cart.png" alt="" />
+          </a>
         </div>
-    );
+        <div className="md:hidden justify-center  " onClick={toggleMenu}>
+          <div className={`${styles.burgermenu} ${isOpen ? styles.open : ""}`}>
+            <div className={`${styles.line} ${styles.line1}`}></div>
+            <div className={`${styles.line} ${styles.line2}`}></div>
+            <div className={`${styles.line} ${styles.line3}`}></div>
+          </div>
+          <BurgerMenu isOpen={isOpen} />
+        </div>
+      </div>
+    </div>
+  );
 }
