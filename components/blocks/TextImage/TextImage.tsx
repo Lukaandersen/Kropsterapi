424a77f5d8fc355c9ae03f49084451f872a0304e
magnuslykke <magn965m@stud.kea.dk>
2024-05-17 13:56:38 +0200

    magnus laver noget

--- components/blocks/TextImage/TextImage.tsx
+++ components/blocks/TextImage/TextImage.tsx
@@ -12,15 +12,9 @@ export default function (props) {
       <div className={`${darkMode ? "text-lightBeige" : "text-primaryPurple"}  p-12 md:col-span-2 grid justify-items-center place-content-center`}>
         <h2 className="text-3xl font-playfair  mb-8">{props.title}</h2>
         <div>
-          <p className="font-josefin max-w-96">
-            For at nå ned til roden af hvad det er der gør ondt og hvorfor det bliver ved med at vende tilbage selvom at man har været en tur til massagekliniken. Så er det formegentligt noget psykisk der presser dig. Og hvis ikke det bliver løst ved
-            roden, så kommer det tilbage.
-          </p>
+          <p className="font-josefin max-w-96"> {props.text}</p>
           <br />
-          <p className="font-josefin max-w-96">
-            Kropetrapi er ideologien om at krop og sind hænger sammen. Som kendt fra fx zoneterapi, så er der flere områder og punkter i vores kroppe som hænger sammen med vores mentale velvære. Grunden til at vælge kropsterapi, er for at komme hele
-            vejen rundt, og komme problemerne til livs.
-          </p>
+          <p className="font-josefin max-w-96">{props.text2}</p>
         </div>
         <a href={props.link}>{darkMode ? <BrownButton text={props.buttonText} /> : <LightBeigeButton text={props.buttonText} />}</a>
       </div>
