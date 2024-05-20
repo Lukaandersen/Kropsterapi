4f7e94a9275b8fdbd49c106f7ae9d0b1efb4fe8c
Luka Andersen <luka_andersen@hotmail.com>
2024-05-20 14:23:32 +0200

    fix: lightbeige button

--- components/buttons/lightBeigeButton.tsx
+++ components/buttons/lightBeigeButton.tsx
@@ -1,12 +1,12 @@
 export default function LightBeigeButton(props) {
-  const handleClick = () => {
-    if (props.onClick) {
-      props.onClick(); 
-  };
+  // const handleClick = () => {
+  //   if (props.onClick) {
+  //     props.onClick(); 
+  // };
 
   return (
     <div className="">
-      <button onClick={props.onClick} className="text-primaryPurple bg-lightBeige font-josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5">
+      <button  className="text-primaryPurple bg-lightBeige font-josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5">
         {props.text}
       </button>
       <style jsx>{`
@@ -17,4 +17,5 @@ export default function LightBeigeButton(props) {
     </div>
   );
 }
-};
+// };
+//onClick={props.onClick}
