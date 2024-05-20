08262be5f6691b703cd4e0cb5e5dfac2fa5dc362
magnuslykke <magn965m@stud.kea.dk>
2024-05-20 16:13:19 +0200

    text ændret

--- components/blocks/DoubleTextBlock/DoubleTextBlock.tsx
+++ components/blocks/DoubleTextBlock/DoubleTextBlock.tsx
@@ -19,9 +19,8 @@ export default function DoubleTextBlock(props) {
       <div className={`${darkMode ? "bg-primaryPurple" : "bg-darkBeige"} p-10 flex flex-col text-primaryLight items-center`}>
         <h2 className="text-xl mb-3 text-center">{props.title2}</h2>
         <p>
-          Kropsterapi er en behandlingsform som består af forskellige teknikker, for eksempel pulsering, tryk, stræk osv. Der er et stort fokus på åndedrættet og der behandles på muskler, meridianbaner, udvalgte zonepunkter mm. Sammenhæng er
-          kropsterapiens tilgang, krop, sind og sjæl om du vil. Det er vigtigt at der er en let tilgængelig forbindelse mellem krop og psyke, fysik og tanke. Alle tilstande har påvirkning på andre dele af dig. Har du ondet et sted i din krop så
-          mærkes det efterhånden også på psyken, tumler du med noget i dit hovede så vise det sig på et tidspunkt også i din krop.
+          Kropsterapi er en sammensat behandlingsform hvor vi taler om hvad der rør sig hos dig når vi starter og hvor du undervejs har mulighed for at sætte ord på det, du mærker i din krop og på de tanker og følelser, der må opstå mens vi
+          behandler. Jeg kan løbende stille spørgsmål som kan åbne op for nye veje i dine tankerækker.
         </p>
         <a href={props.link}>{darkMode ? <PurpleButton text={props.buttonText} /> : <BrownButton text={props.buttonText} />}</a>
       </div>
