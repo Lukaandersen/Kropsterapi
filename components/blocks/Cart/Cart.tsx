1fa69002b161bd44afe9d0b0625430507047d5f6
magnuslykke <magn965m@stud.kea.dk>
2024-05-23 11:47:51 +0200

    knappelap styling

--- components/blocks/Cart/Cart.tsx
+++ components/blocks/Cart/Cart.tsx
@@ -21,7 +21,7 @@ export default function Cart(props) {
       </Link>
       <div className="mt-8 block md:grid md:grid-cols-[2fr,1fr]">
         <CartCardWrapper />
-        <div className="bg-primaryLight m-6 pb-6 text-primaryPurple max-h-[660px]">
+        <div className="bg-primaryLight m-6 pb-6 text-primaryPurple max-h-[720px]">
           <h3 className="text-center text-h3M md:text-h3D py-4 px-4">Dine Betalingsoplysninger</h3>
           <form className="flex flex-col gap-5" onSubmit={bookSlot}>
             <div className="mb-2 flex flex-col">
@@ -48,14 +48,16 @@ export default function Cart(props) {
               </label>
               <textarea id="message" className="bg-gray-300 p-2 ml-4 w-full max-w-[calc(100%-2rem)]" />
             </div>
-            <button>knappelap</button>
+            <button className="text-primaryPurple bg-primaryLight josefin font-bold text-md md:text-xl text-center py-3 px-1 mx-16 rounded-xl shadow-md custom-shadow my-2">{props.button2Text}</button>
+            <style jsx>{`
+              .custom-shadow {
+                box-shadow: -2px -2px 3px rgba(245, 245, 245, 1), 2px 2px 1px rgba(164, 151, 133, 1);
+              }
+            `}</style>
           </form>
           <div className="flex flex-col items-center">
             <h2 className="text-xl pt-6 pb-3">Betalingsmetode</h2>
             <img src="betalingsdut.png" alt="betalingsmetoder" className="pb-0" />
-            <Link href={props.link2}>
-              <LightBeigeButton text={props.button2Text}></LightBeigeButton>
-            </Link>
           </div>
         </div>
       </div>
