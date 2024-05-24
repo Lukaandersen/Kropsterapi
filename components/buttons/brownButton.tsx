f35bc88bcb199746fe378b1f8466e46db4447888
magnuslykke <magn965m@stud.kea.dk>
2024-05-24 12:03:10 +0200

    knapper lavet

--- components/buttons/brownButton.tsx
+++ components/buttons/brownButton.tsx
@@ -1,7 +1,7 @@
 export default function BrownButton(props) {
   return (
     <div className=" bg-darkBeige">
-      <button className="text-lightBeige bg-darkBeige josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5">{props.text}</button>
+      <button className="text-lightBeige bg-darkBeige josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5 transition duration-300 ease-in-out transform hover:scale-110">{props.text}</button>
       <style jsx>{`
         .custom-shadow {
           box-shadow: -2px -2px 3px rgba(209, 202, 186, 1), 2px 2px 1px rgba(35, 35, 35, 1);
--- components/buttons/lightBeigeButton.tsx
+++ components/buttons/lightBeigeButton.tsx
@@ -1,9 +1,7 @@
 export default function LightBeigeButton(props) {
-
-
   return (
     <div className="">
-      <button className="text-primaryPurple bg-lightBeige josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5">{props.text}</button>
+      <button className="text-primaryPurple bg-lightBeige josefin font-bold text-md md:text-xl text-center py-3 px-11 rounded-xl shadow-md custom-shadow mt-5 transition duration-300 ease-in-out transform hover:scale-110">{props.text}</button>
       <style jsx>{`
         .custom-shadow {
           box-shadow: -2px -2px 3px rgba(245, 245, 245, 1), 2px 2px 1px rgba(164, 151, 133, 1);
@@ -12,4 +10,3 @@ export default function LightBeigeButton(props) {
     </div>
   );
 }
-
